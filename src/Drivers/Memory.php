f653de56bc95d0222d43a3d16eaf369d1c7120a0
spinzar <spinzar.inc@gmail.com>
2020-11-03 15:46:42 +0900

    Initial commit

---
+++ src/Drivers/Memory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Spinzar\Setting\Drivers;
+
+use Spinzar\Setting\Contracts\Driver;
+
+class Memory extends Driver
+{
+    /**
+     * @param array $data
+     */
+    public function __construct(array $data = null)
+    {
+        if ($data) {
+            $this->data = $data;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getExtraColumns()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function read()
+    {
+        return $this->data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function write(array $data)
+    {
+        // do nothing
+    }
+}
